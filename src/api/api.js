fc6f70c395e78d2bf1a0bc3c3cc9f86796bf896b
Elio Gerges <elio.gerges2000@gmail.com>
2021-04-12 00:57:22 +0300

    first commit

--- src/api/api.js
+++ src/api/api.js
@@ -49,7 +49,7 @@ try {
     app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.setHeader('Access-Control-Allow-Headers', '*');
         res.setHeader('Access-Control-Allow-Credentials', true);
         next();
     });
